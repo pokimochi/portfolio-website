3a1c6728774f38f20dc4cf7b0c64adab5dde7e83
pokimochi <nolan0597@gmail.com>
2020-01-24 12:31:19 -0500

    projects stable

--- src/App.js
+++ src/App.js
@@ -2,59 +2,64 @@ import React from 'react';
 import './App.css';
 import "@material/layout-grid/mdc-layout-grid.scss";
 import avatar from './images/man.svg';
+import expo1 from './images/expo1.png';
+import ginhawa1 from './images/ginhawa1.png';
+import presentation1 from './images/presentation1.png';
 
 function App() {
   return (
     <div className="app">
-      <div className="app-content">
-        <div id="hero-page">
-          <div id="hero-center">
-            <img id="avatar" alt="Avatar" src={avatar} />
-            <h1>Matthew Deogracias</h1>
-            <h3>Full Stack Developer</h3>
-            <div>
-            </div>
-          </div>
+      <div id="hero-page">
+        <div id="hero-center">
+          <img id="avatar" alt="Avatar" src={avatar} />
+          <div className="hero-title">Matthew Deogracias</div>
+          <div className="hero-sub-title">Full Stack Developer</div>
         </div>
-
+      </div>
+      <div className="app-content">
         <div id="about">
-          <h1>About</h1>
+          <h1 className="section-title">About</h1>
           <span>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse. Sollicitudin ac orci phasellus egestas. In dictum non consectetur a. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
           </span>
         </div>
-
         <div id="projects">
-          <h1>Projects</h1>
-            <div class="mdc-layout-grid">
-              <div class="mdc-layout-grid__inner">
-                <div class="mdc-layout-grid__cell">
-                  <div className="card">
-                    Test
+          <h1 className="section-title">Projects</h1>
+          <div class="mdc-layout-grid" style={{padding: 0}}>
+            <div class="mdc-layout-grid__inner">
+              <div class="mdc-layout-grid__cell">
+                <div className="card">
+                  <img className="card-image" src={presentation1} alt="Intro to Websites Thumbnail" />
+                  <div className="card-content">
+                    <div className="card-title">Intro To Websites</div>
+                    <div className="card-description">Taught the basics of HTML, CSS, and JavaScript by walking through the development of a web-based Tic Tac Toe game.</div>
+                    <a href="https://codepen.io/poki_mochi/pen/oNgVxye" target="_blank" rel="noopener noreferrer" className="card-button">View</a>
                   </div>
                 </div>
-                <div class="mdc-layout-grid__cell">
-                  <div className="card">
-                    Test
+              </div>
+              <div class="mdc-layout-grid__cell">
+                <div className="card">
+                  <img className="card-image" src={expo1} alt="Expo Project Thumbnail" />
+                  <div className="card-content">
+                    <div className="card-title">Miniature Disaster Response Robot</div>
+                    <div className="card-description">A miniature disaster response robot that simulates real-life disaster response robots by locating figures in a wooden maze. Showcased at the University of South Florida's Engineering EXPO 2019.</div>
+                    <a href="https://github.com/Society-of-Competitive-Programmers/Expo2019" target="_blank" rel="noopener noreferrer" className="card-button">View</a>
                   </div>
                 </div>
-                <div class="mdc-layout-grid__cell">
-                  <div className="card">
-                    Test
+              </div>
+              <div class="mdc-layout-grid__cell">
+                <div className="card">
+                  <img className="card-image" src={ginhawa1} alt="Ginhawa Thumbnail" />
+                  <div className="card-content">
+                    <div className="card-title">Non-Profit Website</div>
+                    <div className="card-description">Website that offers rehabilitation for abused women, programs on mental-health therapy, and charity work for indigenous Filipino tribes.</div>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className="card-button">IN-PROGRESS</a>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
         </div>
-
-        <div id="contact">
-          <h1>Contact Me</h1>
-
-        </div>
-      </div>
-
-      <div id="footer">
-
       </div>
     </div>
   );
