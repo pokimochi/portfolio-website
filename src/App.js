65087c5c74c7ebcc9e740cced1c8c5bc420f6dfe
pokimochi <nolan0597@gmail.com>
2020-01-23 21:19:05 -0500

    structure placed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "@material/layout-grid/mdc-layout-grid.scss";
+import avatar from './images/man.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-content">
+        <div id="hero-page">
+          <div id="hero-center">
+            <img id="avatar" alt="Avatar" src={avatar} />
+            <h1>Matthew Deogracias</h1>
+            <h3>Full Stack Developer</h3>
+            <div>
+            </div>
+          </div>
+        </div>
+
+        <div id="about">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse. Sollicitudin ac orci phasellus egestas. In dictum non consectetur a. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
+          </span>
+        </div>
+
+        <div id="projects">
+          <h1>Projects</h1>
+            <div class="mdc-layout-grid">
+              <div class="mdc-layout-grid__inner">
+                <div class="mdc-layout-grid__cell">
+                  <div className="card">
+                    Test
+                  </div>
+                </div>
+                <div class="mdc-layout-grid__cell">
+                  <div className="card">
+                    Test
+                  </div>
+                </div>
+                <div class="mdc-layout-grid__cell">
+                  <div className="card">
+                    Test
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div id="contact">
+          <h1>Contact Me</h1>
+
+        </div>
+      </div>
+
+      <div id="footer">
+
+      </div>
     </div>
   );
 }
